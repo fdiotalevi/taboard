c70cbd4d6c8e4ab0273c4c8e74ab31f96c96d738
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2011-05-27 22:31:27 +0100

    initial commit

---
+++ src/org/taboard/fragments/GitCommits.java
@@ -0,0 +1,101 @@
+package org.taboard.fragments;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+public class GitCommits extends ListFragment{
+	
+	private static final String TAG = "gitcommits";
+	private String URL = "http://github.com/api/v2/json/commits/list/mojombo/grit/master";
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+	}
+	
+   @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        
+        getCommitList();
+        
+        // Populate list with our static array of titles.
+        setListAdapter(new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_activated_1, new String[] {"1", "2", "3"}));
+                
+    }
+   
+   	private void getCommitList() {
+   		
+   		AsyncTask<String, Void, List<Commit>> task = new AsyncTask<String, Void, List<Commit>>() {
+
+			@Override
+			protected List<Commit> doInBackground(String... params) {
+				HttpClient client = new DefaultHttpClient();
+		   		HttpGet get = new HttpGet(params[0]);
+		   		
+		   		try {
+		   			BasicResponseHandler handler = new BasicResponseHandler();
+					String response = client.execute(get, handler);
+														
+					@SuppressWarnings("unused")
+					JSONObject commits = new JSONObject(response); 
+										
+					JSONArray commitArray = commits.getJSONArray("commits");
+					
+					List<Commit> output = new ArrayList<Commit>();
+					
+					for (int i = 0; i < commitArray.length(); i++) {
+						output.add(new Commit((JSONObject) commitArray.get(i)));
+					}
+					
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (Throwable e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			}
+	
+   			
+   		};
+   		
+   		task.execute(URL);
+   		
+   		
+   		
+   		
+   	}
+   
+   
+   
+
+}
