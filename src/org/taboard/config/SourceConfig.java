628b828f1526478bd1701fa43a06443c8b86437b
Friedger <friedger@gmail.com>
2011-05-30 10:49:52 +0200

    comment

--- src/org/taboard/config/SourceConfig.java
+++ src/org/taboard/config/SourceConfig.java
@@ -5,6 +5,13 @@ import org.taboard.SourceManager;
 import android.app.Activity;
 import android.app.Fragment;
 
+/**
+ * Configuration of a data source toe should be shown in a Fragment of type T
+ * 
+ * @author friedger
+ *
+ * @param <T> type of Fragment to be used for displaying this data
+ */
 public interface SourceConfig<T extends Fragment>{
 
 	T createFragment(Activity activity, SourceManager sourceManager);
